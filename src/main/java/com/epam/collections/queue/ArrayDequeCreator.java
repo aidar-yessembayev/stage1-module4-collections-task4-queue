c61f404e5fb662231f0fa65b77d5e7a4a4e07a77
aidar-yessembayev <98548293+aidar-yessembayev@users.noreply.github.com>
2023-01-30 13:03:35 +0600

    Update ArrayDequeCreator.java

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,43 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> result = new ArrayDeque<>();
+
+        // Make from Queue Deque
+        Deque<Integer> first = new LinkedList<>();
+        Deque<Integer> second = new LinkedList<>();
+
+        for (int i : firstQueue) {
+            first.add(i);
+        }
+
+        for (int i : secondQueue) {
+            second.add(i);
+        }
+
+        // Both take turns adding two cards to the ArrayDeque<Integer>
+        result.addLast(first.pollFirst());
+        result.addLast(first.pollFirst());
+
+        result.addLast(second.pollFirst());
+        result.addLast(second.pollFirst());
+
+        while (!(first.isEmpty() && second.isEmpty())) {
+            // Takes the top card from the result and adds this card to the end of his first and last
+            first.addLast(result.pollLast());
+            second.addLast(result.pollLast());
+
+            // Puts two cards from the beginning of his first and second into the result
+            result.addLast(first.pollFirst());
+            result.addLast(first.pollFirst());
+
+            result.addLast(second.pollFirst());
+            result.addLast(second.pollFirst());
+        }
+
+        return result;
     }
 }
