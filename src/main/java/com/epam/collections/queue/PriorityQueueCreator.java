294851867dd33000585ec5a7bd201cd4a7e98396
aidar-yessembayev <98548293+aidar-yessembayev@users.noreply.github.com>
2023-01-30 13:04:46 +0600

    Update PriorityQueueCreator.java

--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,34 @@
 package com.epam.collections.queue;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        PriorityQueue<String> first = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<String> last = new PriorityQueue<>(Comparator.reverseOrder());
+
+        if (!(firstList.isEmpty() && secondList.isEmpty())) {
+
+            if (!firstList.isEmpty()) {
+                for (String i : firstList) {
+                    first.add(i);
+                }
+            }
+
+            if (!secondList.isEmpty()) {
+                for (String i : secondList) {
+                    first.add(i);
+                }
+            }
+
+            int size = first.size();
+            for (int i = 0; i < size; i++) {
+                last.add(first.poll());
+            }
+        }
+
+        return last;
     }
 }
